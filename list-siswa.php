d8a374fdf73550b801fa903d6685a4a449ac4063
Muhammad Rafi Insan Fillah <78022264+Mengz04@users.noreply.github.com>
2023-04-17 08:24:22 +0700

    Update list-siswa.php

--- list-siswa.php
+++ list-siswa.php
@@ -6,13 +6,14 @@
 <head>
 	<title>Pendaftaran Siswa Baru | SMK Coding</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="./mystyle.css">
 </head>
 
 <body class="bg-dark mt-5">
-	<div class="container col-md-8 bg-light align-center p-4">
+	<div class="container col-md-10 col-sm-12 bg-light align-center p-4">
 		<div class="col align-center">
 			<header>
-				<h3>Siswa yang sudah mendaftar</h3>
+				<h3 class="text-center gothBL">Siswa yang sudah mendaftar</h3>
 			</header>
 			
 			<nav>
