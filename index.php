a0060247eb0265034bb4b1dcbbeddbab6fb25776
Muhammad Rafi Insan Fillah <78022264+Mengz04@users.noreply.github.com>
2023-04-17 08:24:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,19 @@
 <head>
 	<title>Pendaftaran Siswa Baru | SMK Coding</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="./mystyle.css">
+
 </head>
 
 <body class="bg-dark mt-5">
 	<div class="container col-md-6 bg-light align-center p-4">
 		<div class="col align-center">
 			<header>
-				<h3>Pendaftaran Siswa Baru</h3>
-				<h1>SMK Coding</h1>
+				<h3 class="gothBL">Pendaftaran Siswa Baru</h3>
+				<h1 class="gothBL">SMK Coding</h1>
 			</header>
 			
-			<h4>Menu</h4>
+			<h5>Menu</h4>
 			<nav>
 				<ul>
 					<li><a href="form-daftar.php">Daftar Baru</a></li>
